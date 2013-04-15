fe113c72134de12c6a14f96177bafc1548ad0349
Greg Bowser <topnotcher@gmail.com>
2013-04-15 19:57:14 -0400

    meh

---
+++ shoulder/config.h
@@ -0,0 +1,15 @@
+#ifndef BOARD_CONFIG_H
+
+/**
+ * Configuration specific to this board as required to work with includes etcetc
+ */
+
+#define LED_PORT PORTC
+#define LED_SCLK PIN7_bm
+#define LED_SOUT PIN5_bm
+
+
+#define LED_RB_SWAP 1 
+
+
+#endif
---
+++ shoulder/main.c
@@ -0,0 +1,66 @@
+#include <avr/io.h>
+#include <util/delay.h>
+#include <stdint.h>
+#include <avr/interrupt.h>
+#include <avr/sleep.h>
+#include <stdlib.h>
+
+
+/**
+ * G4 common includes.
+ */
+#include <leds.h>
+
+#define CLKSYS_Enable( _oscSel ) ( OSC.CTRL |= (_oscSel) )
+
+#define CLKSYS_IsReady( _oscSel ) ( OSC.STATUS & (_oscSel) )
+
+
+int main(void) {
+
+	// Enable 32 MHz Osc. 
+	CLKSYS_Enable( OSC_RC32MEN_bm ); 
+	// Wait for 32 MHz Osc. to stabilize 
+	while ( CLKSYS_IsReady( OSC_RC32MRDY_bm ) == 0 ); 
+	// was 8  PLL mult. factor ( 2MHz x8 ) and set clk source to PLL. 
+	OSC_PLLCTRL = 16;  
+
+	//enable PLL
+	OSC_CTRL = 0x10;
+
+	//wait for PLL clk to be ready
+	while( !( OSC_STATUS & 0x10 ) );
+
+	//Unlock seq. to access CLK_CTRL
+	CCP = 0xD8; 
+
+	// Select PLL as sys. clk. These 2 lines can ONLY go here to engage the PLL ( reverse of what manual A pg 81 says )
+	CLK_CTRL = 0x04;
+
+	PMIC.CTRL |= PMIC_MEDLVLEN_bm;
+
+
+	led_config_t led_config = { &PORTC, PIN5_bm, PIN7_bm };
+
+/*	led_config.port = &PORTD;
+	led_config.sout = PIN5_bm;
+	led_config.sclk = PIN7_bm;*/
+
+	//safe to pass PTR because we never leave main()
+	led_init(&led_config);
+//	PORTC.DIRSET |= 0xff;
+
+//	sei();
+	set_lights(1);
+
+
+	PORTA.DIRSET |= 0xFF;
+
+	while(1) {
+
+//		_delay_ms(10);
+		leds_run();
+	}
+
+	return 0;
+}
