7d303e9961ba4898c85f9c8be9cb97cd36b71d82
Greg Bowser <topnotcher@gmail.com>
2013-04-16 18:06:30 -0400

    add the chest

---
+++ chest/config.h
@@ -0,0 +1,13 @@
+#ifndef BOARD_CONFIG_H
+
+/**
+ * Configuration specific to this board as required to work with includes etcetc
+ */
+
+#define LED_PORT PORTC
+#define LED_SCLK PIN7_bm
+#define LED_SOUT PIN5_bm
+
+
+
+#endif
