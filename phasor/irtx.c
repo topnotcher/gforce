25ce364d2c15ce317a1a8d536247a23b840f11d3
Greg Bowser <topnotcher@gmail.com>
2013-04-20 22:45:37 -0400

    this works to trigger an interrupt when the value changes, we think...

--- phasor/irtx.c
+++ phasor/irtx.c
@@ -1,16 +1,22 @@
 #include <avr/io.h>
+#include <avr/interrupt.h>
 
 #include "config.h"
+
 #include <util.h>
+#include <leds.h>
 
 #include "irtx.h"
 
-#define IRTX_USART_TXPIN_bm G4_CONCAT(PIN,IRTX_USART_TXPIN,_bm)
-
+/**
+ * Magic macros to minimize my configuration.
+ */
 #define _PIN(id) G4_CONCAT3(PIN,id,_bm)
 #define _PINCTRL(id) G4_CONCAT3(PIN,id,CTRL)
 #define _TXPIN_bm _PIN(IRTX_USART_TXPIN)
 #define _TXPINCTRL _PINCTRL(IRTX_USART_TXPIN)
+
+
 inline void irtx_init() {
 	
 	IRTX_USART_PORT.OUTCLR = _TXPIN_bm;
@@ -18,16 +24,23 @@ inline void irtx_init() {
 	
 	IRTX_USART_PORT._TXPINCTRL |= PORT_INVEN_bm;
 
+	//configure interrupts
+	IRTX_USART_PORT.INT0MASK = _TXPIN_bm;
+	IRTX_USART_PORT.INTCTRL = PORT_INT0LVL_MED_gc;
+	//end interrupts
+	
 	IRTX_USART.BAUDCTRLA = (uint8_t)(IRTX_USART_BSEL&0x00FF);
 	IRTX_USART.BAUDCTRLB = (IRTX_USART_BSCALE<<USART_BSCALE_gp) | (uint8_t)( (IRTX_USART_BSEL>>8) & 0x0F );
 
 	IRTX_USART.CTRLA = 0;
 	IRTX_USART.CTRLB = USART_TXEN_bm;
 	IRTX_USART.CTRLC = USART_PMODE_DISABLED_gc | USART_CHSIZE_8BIT_gc;
-
-
 }
 
 inline void irtx_byte(uint8_t byte) {
 	IRTX_USART.DATA = byte;
+}
+
+ISR(PORTD_INT0_vect) {
+
 }
