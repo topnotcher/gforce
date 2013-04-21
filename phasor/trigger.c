bd3a73363fc7bef965f5e6d9b7f745b1362d9218
Greg Bowser <topnotcher@gmail.com>
2013-04-21 13:39:04 -0400

    Add functions to en/disable trigger

--- phasor/trigger.c
+++ phasor/trigger.c
@@ -1,3 +1,4 @@
+#include <stdbool.h>
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
@@ -8,13 +9,20 @@
 #define _TRIGPINCTRL G4_PINCTRL(TRIGGER_PIN)
 #define _TRIGPIN_bm G4_PIN(TRIGGER_PIN)
 
-static bool _trigger_pressed;
+static bool trigger_enabled(void);
+static void trigger_disable(void);
+static void trigger_enable(void);
+
+
+static volatile bool _trigger_pressed;
+static volatile bool _trigger_enabled;
 
 inline void trigger_init() {
 	TRIGGER_PORT.DIRCLR = _TRIGPIN_bm;
-	TRIGGER_PORT._TRIGPINCTRL = PORT_OPC_PULLUP_gc | PORT_ISC_FALLING_gc;
+	TRIGGER_PORT._TRIGPINCTRL = PORT_OPC_PULLUP_gc;
 
 	_trigger_pressed = false;
+	_trigger_enabled = true;
 
 	//configure interrupts
 	TRIGGER_PORT.INT0MASK = _TRIGPIN_bm;
@@ -28,8 +36,17 @@ inline bool trigger_pressed() {
 	}
 	return false;
 }
+static inline void trigger_enable() {
+	_trigger_enabled = true;
+}
+static inline void trigger_disable() {
+	_trigger_enabled = false;
+}
+static inline bool trigger_enabled() {
+	return _trigger_enabled;
+}
 
 ISR(PORTA_INT0_vect) {
-	if ( ! (TRIGGER_PORT.IN & _TRIGPIN_bm) )
+	if ( !(TRIGGER_PORT.IN & _TRIGPIN_bm) &&  trigger_enabled() ) 
 		_trigger_pressed = true;
 }
