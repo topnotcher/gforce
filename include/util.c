852d0156703557c957bb390acaeda51a85993965
Greg Bowser <topnotcher@gmail.com>
2013-04-22 21:19:45 -0400

    minor crc change

--- include/util.c
+++ include/util.c
@@ -2,18 +2,15 @@
 #include <util.h>
 
 void crc(uint8_t * const shift, uint8_t byte, const uint8_t poly) {
-	//byte &= 0xff;
 	uint8_t fb;
 
 	for ( uint8_t i = 0; i < 8; i++) {
-		fb = (*shift ^ byte) & 0x01; // one bit
+		fb = (*shift ^ byte) & 0x01; 
 
 		if ( fb )
-			*shift = (*shift) ^ poly;
-
-		*shift >>= 1;
-		*shift = ( (*shift) | (fb << 7) );
+			*shift = 0b10000000|(((*shift) ^ poly)>>1);
+		else 
+			*shift >>= 1;
 		byte >>= 1;
 	}
 }
-
