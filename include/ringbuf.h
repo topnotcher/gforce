bc82c96e2b5c501dc350e2cc02293ddbf8973164
Greg Bowser <topnotcher@gmail.com>
2013-05-11 22:23:30 -0400

    Cuts a ton of instructions out of the UART RXC ISRs

--- include/ringbuf.h
+++ include/ringbuf.h
@@ -3,6 +3,14 @@
 #ifndef RINGBUF_H
 #define RINGBUF_H
 
+#define ringbuf_next_idx(idx,size) ((idx == size-1) ? 0 : idx+1)
+
+//ringbuf_t * buf
+#define ringbuf_full(buf) (ringbuf_next_idx(buf->write, buf->size) == buf->read)
+
+//ringbuf_t * buf
+#define ringbuf_empty(buf) (buf->read == buf->write)
+
 typedef struct {
 	volatile uint8_t read;
 	volatile uint8_t write;
@@ -12,12 +20,24 @@ typedef struct {
 
 ringbuf_t * ringbuf_init(uint8_t size);
 void ringbuf_deinit(ringbuf_t *);
-void ringbuf_put(ringbuf_t * const buf, uint8_t data);
+//void ringbuf_put(ringbuf_t * const buf, uint8_t data);
 uint8_t ringbuf_get(ringbuf_t *);
 
-uint8_t ringbuf_empty(ringbuf_t *);
-uint8_t ringbuf_full(ringbuf_t *);
+//uint8_t ringbuf_empty(ringbuf_t *);
+//uint8_t ringbuf_full(ringbuf_t *);
 
 void ringbuf_flush(ringbuf_t *);
+
+inline __attribute__((always_inline)) void ringbuf_put(ringbuf_t * const buf, uint8_t data) {
+	
+	//queue is full. (note: this wastes the current position in the queue.)
+	//if full condition is not detected, then overflow will dump the entire queue (meh, just make the queue larger?)
+	//if ( ringbuf_full(buf) ) 
+	//	return;
+
+
+	buf->data[buf->write] = data;
+	buf->write = ringbuf_next_idx(buf->write,buf->size);
+}
 
 #endif
