51bec509e58f38c1b27d69b8ec53d7e11bbf9432
Greg Bowser <topnotcher@gmail.com>
2013-09-02 22:02:26 -0400

    cleanup

--- master/main.c
+++ master/main.c
@@ -52,57 +52,21 @@ int main(void) {
 	CLK_CTRL = 0x04;
 
 	scheduler_init();
-	//lights_init();
-//	ir_sensor_init();
-//	lcd_init();
 	scheduler_init();
 	sound_init();
 	xbee_init();
 	phasor_comm_init();
-//	game_init();
 	mpc_init();
 	display_init();
 
 	PMIC.CTRL |= PMIC_MEDLVLEN_bm | PMIC_LOLVLEN_bm | PMIC_HILVLEN_bm;
 	sei();
 
-	#if DEBUG == 0
-	/** 
-	 * Setting all three bits is extended standby.
-	 * Setting no bits is idle. Dicks get sucked if you try to
-	 * use USART in extended standby. (no wakey wakey!!)
-	 */
-//	SMCR = /*_BV(SM1) |  _BV(SM2) | _BV(SM0) |*/ _BV(SE);
-	#endif
-//	uint8_t led_pattern[] = {8,0,16,16,16,16,1,6,0,2,2,2,2,1,6,0,48,48,48,48,1,6,0,6,6,6,6,1,6,0,128,128,128,128,1,6,0,15,15,15,15,1,6,0,112,112,112,112,1,6,0,5,5,5,5,1,6,0};
-//	uint8_t led_pattern_size = 58;
-//	uint8_t led_pattern[] = {1, 1, (COLOR_RED<<4 | COLOR_BLUE) , (COLOR_ORANGE<<4 | COLOR_CYAN) , (COLOR_PINK<<4 | COLOR_GREEN) , (COLOR_PURPLE<<4 | COLOR_YELLOW), 10, 15, 15};	
-//	uint8_t led_pattern_size = 9;
-
 	while (1) {
-	
 
-/*		 mpc_send(0b0001,'A', led_pattern, led_pattern_size);
-		 _delay_ms(2500);
-		 mpc_send(0b0010,'A', led_pattern, led_pattern_size);
-		 _delay_ms(2500);
-		 mpc_send(0b0100,'A', led_pattern, led_pattern_size);
-		 _delay_ms(2500);
-		 mpc_send(0b1000,'A', led_pattern, led_pattern_size);
-		 _delay_ms(2500);
-		 phasor_comm_send('A',led_pattern, led_pattern_size);
-		_delay_ms(2500);
-
-		 mpc_send_cmd(0b1111,'B');
-		 phasor_comm_send('B', NULL, 0);
-*/
 		process_ib_pkt(xbee_recv());
-//		process_ib_pkt(mpc_recv());
-//		process_ib_pkt(phasor_comm_recv());
-
-		#if DEBUG == 0
-//		sleep_cpu();
-		#endif
+		process_ib_pkt(mpc_recv());
+		process_ib_pkt(phasor_comm_recv());
 	}
 
 	return 0;
@@ -110,9 +74,6 @@ int main(void) {
 
 
 static inline void process_ib_pkt(mpc_pkt * pkt) {
-//	uint8_t led_pattern[] = {8,0,16,16,16,16,1,6,0,2,2,2,2,1,6,0,48,48,48,48,1,6,0,6,6,6,6,1,6,0,128,128,128,128,1,6,0,15,15,15,15,1,6,0,112,112,112,112,1,6,0,5,5,5,5,1,6,0};
-//	uint8_t led_pattern_size = 58;
-
 
 	if ( pkt == NULL ) return;
 	
@@ -121,9 +82,6 @@ static inline void process_ib_pkt(mpc_pkt * pkt) {
 
 	} else if ( pkt->cmd == 'I' && pkt->data[0] == 0x08 )  {
 		stop_game_cmd((command_t*)pkt->data);
-//		phasor_comm_send('B',NULL,0);
-//		mpc_send_cmd(0b1111,'B');
-//		on = 0;
 	} else if ( pkt->cmd == 'I' && pkt->data[0] == 0x0c ) {
 
 		char * sensor;
@@ -156,8 +114,6 @@ static inline void process_ib_pkt(mpc_pkt * pkt) {
 		else
 			do_stun();
 	}
-
-		//set_lights(1);
 
 	free(pkt);
 }
