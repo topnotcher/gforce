f7deabb72368435861081e720bf979f3587661b4
Greg Bowser <topnotcher@gmail.com>
2013-09-20 19:24:23 -0400

    this led timing looks better

--- master/lights.c
+++ master/lights.c
@@ -7,7 +7,7 @@
 static uint8_t led_stun_pattern[] = {8,0,16,16,16,16,1,6,0,2,2,2,2,1,6,0,48,48,48,48,1,6,0,6,6,6,6,1,6,0,128,128,128,128,1,6,0,15,15,15,15,1,6,0,112,112,112,112,1,6,0,5,5,5,5,1,6,0};
 static uint8_t led_stun_pattern_size = 58;
 
-static uint8_t led_active_pattern[] = {2,1, 0x01,0x01,0x01,0x01, 1,15,15, 0x10,0x10,0x10,0x10, 1,15,15 };
+static uint8_t led_active_pattern[] = {2,0, 0x1,0x01,0x01,0x01, 1,15,0, 0x10,0x10,0x10,0x10, 1,15,0 };
 static uint8_t led_active_pattern_size = 16;
 
 
