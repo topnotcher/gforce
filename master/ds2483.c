0dfabeba669e4de8a5427da942171838ecca9217
Greg Bowser <topnotcher@gmail.com>
2014-03-06 15:11:44 -0500

    Document functions for d2483

--- master/ds2483.c
+++ master/ds2483.c
@@ -41,6 +41,13 @@ static inline void ds2483_write_read(ds2483_dev_t * dev, uint8_t txbytes, uint8_
 	twi_master_write_read(dev->twim, DS2483_I2C_ADDR, txbytes, txbuf, rxbytes, rxbuf);
 }
 
+/**
+ * Initiates reads on the one wire bus, and reads a single byte, 
+ * which is stored in the read data register. The read data register
+ * is then fetched and returned to the caller.
+ *
+ * @return a byte read from the one wire bus
+ */
 uint8_t ds2483_1w_read_byte(ds2483_dev_t * dev) {
 	dev->cmd[0] = DS2483_CMD_1W_READ_BYTE;
 	ds2483_write(dev, 1, dev->cmd);
@@ -48,6 +55,9 @@ uint8_t ds2483_1w_read_byte(ds2483_dev_t * dev) {
 	return ds2483_read_register(dev, DS2483_REGISTER_READ_DATA);
 }
 
+/*
+ * Writes a byte to the one wire bus.
+ */
 void ds2483_1w_write(ds2483_dev_t * dev, uint8_t data) {
 	dev->cmd[0] = DS2483_CMD_1W_WRITE_BYTE;
 	dev->cmd[1] = data;
@@ -55,17 +65,32 @@ void ds2483_1w_write(ds2483_dev_t * dev, uint8_t data) {
 	ds2483_1w_wait_idle(dev);
 }
 
+/**
+ * Sets the read pointer on the DS2483. Subsequent read attempts
+ * to the device will read from the specified register.
+ */
 void ds2483_set_read_ptr(ds2483_dev_t * dev, uint8_t reg) {
 	dev->cmd[0] = DS2483_CMD_SET_READ_PTR;
 	dev->cmd[1] = reg;
 	ds2483_write(dev, 2, dev->cmd);
 }
 
+/** 
+ * Reads a single byte from the DS2483, read from the register
+ * set by ds2483_set_read_ptr
+ * 
+ * @return the byte read
+ */
 uint8_t ds2483_read_byte(ds2483_dev_t * dev) {
 	ds2483_read(dev, 1, (uint8_t*)&dev->result);
 	return dev->result;
 }
 
+/**
+ * Sets the read pointer and reads a single byte in one transaction.
+ *
+ * @return the value of the requested register
+ */
 uint8_t ds2483_read_register(ds2483_dev_t * dev, uint8_t reg) {
 	dev->cmd[0] = DS2483_CMD_SET_READ_PTR;
 	dev->cmd[1] = reg;
@@ -81,6 +106,9 @@ uint8_t ds2483_last_data(ds2483_dev_t*dev) {
 	return dev->result;
 }
 
+/**
+ * Resets the DS2483
+ */
 void ds2483_rst(ds2483_dev_t * dev) {
 	dev->cmd[0] = DS2483_CMD_RST;
 	ds2483_write(dev,1,&dev->cmd[0]);
@@ -88,6 +116,7 @@ void ds2483_rst(ds2483_dev_t * dev) {
 
 /**
  * Performs a reset/presence detect
+ *
  * @return 1 if device present, 0 otherwise
  */
 uint8_t ds2483_1w_rst(ds2483_dev_t * dev) {
@@ -97,6 +126,8 @@ uint8_t ds2483_1w_rst(ds2483_dev_t * dev) {
 }
 
 /**
+ * Waits for any ongoing one wire bus activity to finish.
+ *
  * @return The contents of the status register
  */
 static uint8_t ds2483_1w_wait_idle(ds2483_dev_t * dev) {
