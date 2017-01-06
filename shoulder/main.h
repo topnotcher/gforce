a6dfd148bb2d19ccefababfccb2a2fbcb176da3e
Greg Bowser <topnotcher@gmail.com>
2017-01-06 18:34:31 -0500

    Remove useless externs...
    Why were those there?

--- shoulder/main.h
+++ shoulder/main.h
@@ -4,27 +4,27 @@
 #define system_init_func(fn) void fn(void)
 #define system_init_stub(fn) system_init_func(fn) __attribute__((weak, alias("_nopfn")))
 
-extern system_init_func(_nopfn);
+system_init_func(_nopfn);
 
 /**
  * Initialize system block, low level board hardware, etc.
  */
-extern system_init_func(system_board_init);
+system_init_func(system_board_init);
 
 /**
  * Configure the interrupt controller. Leave interrutps disabled - they will be
  * enabled when a task starts.
  */
-extern system_init_func(system_configure_interrupts);
+system_init_func(system_configure_interrupts);
 
 /**
  * Configure the OS timer interrupt and associated clock source.
  */
-extern system_init_func(system_configure_os_ticks);
+system_init_func(system_configure_os_ticks);
 
 /**
  * Initialize software components - called immediately before starting the scheduler.
  */
-extern system_init_func(system_software_init);
+system_init_func(system_software_init);
 
 #endif
