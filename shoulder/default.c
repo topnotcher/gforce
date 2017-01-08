0052c5430d01ea2e6b86f9726f856ce6b1d968bd
Greg Bowser <topnotcher@gmail.com>
2017-01-07 20:23:20 -0500

    Add missing stub.

--- shoulder/default.c
+++ shoulder/default.c
@@ -7,6 +7,6 @@ void _nopfn(void) {}
  * system_board_init: first function called in main() - sets up clocks etc.
  */
 system_init_stub(system_board_init);
-system_init_stub(system_interrupts_enable);
+system_init_stub(system_configure_interrupts);
 system_init_stub(system_configure_os_ticks);
 system_init_stub(system_software_init);
