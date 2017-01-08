854ab43d0aa3a07bd0de0d79279c3c4f9d43bc16
Greg Bowser <topnotcher@gmail.com>
2017-01-08 12:04:18 -0500

    Modify LED blinker to exercise scheduler.

--- shoulder/saml21.c
+++ shoulder/saml21.c
@@ -1,17 +1,24 @@
 #include <stdint.h>
 #include <saml21/io.h>
 
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+
 #include "main.h"
 
-system_init_func(system_software_init) {
+static void led_task(void *);
 
+system_init_func(system_software_init) {
 	// LED0 on xplained board.
 	PORT[0].Group[1].DIRSET.reg = 1 << 10;
 	PORT[0].Group[1].OUTCLR.reg = 1 << 10;
 
-	while (1) {
-		for (volatile uint64_t i = 0; i < 100000; ++i);
+	xTaskCreate(led_task, "led task", 256, NULL, tskIDLE_PRIORITY + 1, NULL);
+}
 
+static void led_task(void *params) {
+	while (1) {
 		PORT[0].Group[1].OUTTGL.reg = 1 << 10;
+		vTaskDelay(configTICK_RATE_HZ / 2);
 	}
 }
