f164719979e4a7d9249a123a452d16aecfe715a4
Greg Bowser <topnotcher@gmail.com>
2017-01-07 19:51:46 -0500

    fixup! Use port(ENTER|EXIT)_CRITICAL() instead of avr-gcc's ATOMIC_BLOCK().

--- common/mempool.c
+++ common/mempool.c
@@ -1,5 +1,4 @@
 #include <malloc.h>
-#include <util/atomic.h>
 
 #include "mempool.h"
 
@@ -26,7 +25,7 @@ mempool_t *init_mempool(const uint8_t block_size, const uint8_t size) {
 void *mempool_alloc(mempool_t *pool) {
 	void *ret = NULL;
 
-	ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
+	portENTER_CRITICAL(); {
 		for (uint8_t i = 0; i < pool->size; ++i) {
 			mempool_block_t *block = block(pool, i);
 			if (block->refcnt == 0) {
@@ -35,7 +34,7 @@ void *mempool_alloc(mempool_t *pool) {
 				break;
 			}
 		}
-	}
+	} portEXIT_CRITICAL();
 
 	return ret;
 }
