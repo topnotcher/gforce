cdb0b6015c9933433d9eb654ceb94f72fd9cc34f
Greg Bowser <topnotcher@gmail.com>
2017-01-29 14:38:09 -0500

    xmega: Add interrupt handling to DMA driver.

--- drivers/xmega/dma.c
+++ drivers/xmega/dma.c
@@ -1,8 +1,11 @@
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <assert.h>
 
 #include <avr/io.h>
+
+#include <drivers/xmega/dma.h>
 
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
@@ -10,20 +13,28 @@
 #define DMA_CHAN_NUM 4
 
 static void dma_init(void);
+static inline void dma_handler(uint8_t) __attribute__((always_inline));
 
 static uint8_t dma_channel_alloc_mask;
+
+static struct {
+	void (*handler)(void *);
+	void *ins;
+} dma_vector_table[DMA_CHAN_NUM];
 
 static void dma_init(void) {
 	static bool dma_initialized;
 
 	if (!dma_initialized) {
+		DMA.CTRL &= ~DMA_ENABLE_bm;
+
+		DMA.CTRL = DMA_RESET_bm;
 		DMA.CTRL |= DMA_ENABLE_bm;
 	}
 }
 
 DMA_CH_t *dma_channel_alloc(void) {
 	dma_init();
-	DMA_CH_t *dma_channels = &DMA.CH0;
 	DMA_CH_t *chan = NULL;
 
 	vTaskSuspendAll();
@@ -31,10 +42,45 @@ DMA_CH_t *dma_channel_alloc(void) {
 		if (!(dma_channel_alloc_mask & (1 << i))) {
 			dma_channel_alloc_mask |= 1 << i;
 
-			chan = dma_channels + i;
+			chan = &DMA.CH0 + i;
+
+			chan->CTRLA &= ~DMA_CH_ENABLE_bm;
+			chan->CTRLA = DMA_CH_RESET_bm;
 		}
 	}
 	xTaskResumeAll();
 
 	return chan;
 }
+
+int8_t dma_get_channel_index(const DMA_CH_t *const chan) {
+
+	for (int8_t i = 0; i < DMA_CHAN_NUM; ++i) {
+		if (chan == (&DMA.CH0 + i))
+			return i;
+	}
+
+	return -1;
+}
+
+void dma_register_handler(uint8_t dma_chan_index, void (*handler)(void *), void *ins) {
+	dma_vector_table[dma_chan_index].handler = handler;
+	dma_vector_table[dma_chan_index].ins = ins;
+}
+
+static inline void dma_handler(uint8_t idx) {
+	dma_vector_table[idx].handler(dma_vector_table[idx].ins);
+}
+
+#define CONCAT(a, b, c) a ## b ## c
+#define DMA_HANDLER(x) \
+void __attribute__((signal)) DMA_CH ## x ## _vect(void) { \
+	dma_handler(x); \
+}
+
+DMA_HANDLER(0)
+DMA_HANDLER(1)
+DMA_HANDLER(2)
+DMA_HANDLER(3)
+
+//static_assert(DMA_CHAN_NUM == 4, "Expected 4 dma channels!");
--- include/drivers/xmega/dma.h
+++ include/drivers/xmega/dma.h
@@ -5,7 +5,8 @@
 
 DMA_CH_t *dma_channel_alloc(void);
 
-inline __attribute__((always_inline)) void dma_chan_set_destaddr(DMA_CH_t *const chan, const void *const addr) {
+inline __attribute__((always_inline, nonnull(1, 2)))
+void dma_chan_set_destaddr(DMA_CH_t *const chan, const void *const addr) {
 	#pragma GCC diagnostic push
 	#pragma GCC diagnostic ignored "-Wpointer-to-int-cast"
 	chan->DESTADDR0 = ((uint32_t)addr) & 0xFF;
@@ -14,7 +15,8 @@ inline __attribute__((always_inline)) void dma_chan_set_destaddr(DMA_CH_t *const
 	#pragma GCC diagnostic pop
 }
 
-inline __attribute__((always_inline)) void dma_chan_set_srcaddr(DMA_CH_t *const chan, const void *const addr) {
+inline __attribute__((always_inline, nonnull(1, 2)))
+void dma_chan_set_srcaddr(DMA_CH_t *const chan, const void *const addr) {
 	#pragma GCC diagnostic push
 	#pragma GCC diagnostic ignored "-Wpointer-to-int-cast"
 	chan->SRCADDR0 = ((uint32_t)addr) & 0xFF;
@@ -23,12 +25,17 @@ inline __attribute__((always_inline)) void dma_chan_set_srcaddr(DMA_CH_t *const 
 	#pragma GCC diagnostic pop
 }
 
-inline __attribute__((always_inline)) void dma_chan_set_transfer_count(DMA_CH_t *const chan, uint16_t count) {
+inline __attribute__((always_inline, nonnull(1)))
+void dma_chan_set_transfer_count(DMA_CH_t *const chan, uint16_t count) {
 	chan->TRFCNTL = count & 0xFF;
 	chan->TRFCNTH = (count >> 8) & 0xFF;
 }
 
-inline __attribute__((always_inline)) void dma_chan_enable(DMA_CH_t *const chan) {
+inline __attribute__((always_inline, nonnull(1)))
+void dma_chan_enable(DMA_CH_t *const chan) {
 	chan->CTRLA |= DMA_CH_ENABLE_bm;
 }
+
+int8_t dma_get_channel_index(const DMA_CH_t *) __attribute__((nonnull(1), const));
+void dma_register_handler(uint8_t, void (*)(void *), void *) __attribute__((nonnull(2)));
 #endif
