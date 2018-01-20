4929c410700893ff86196c44e943daae2edc851d
Greg Bowser <topnotcher@gmail.com>
2018-01-20 17:34:38 -0500

    sercom: Support SAMD51 clocks and DMA trigger sources.

--- drivers/saml21/sercom.c
+++ drivers/saml21/sercom.c
@@ -55,11 +55,25 @@ void sercom_disable_irq(const int sercom_index) {
 }
 
 void sercom_enable_pm(int sercom_index) {
-	if (sercom_index != 5) {
+
+// TODO: this would work better with a series define
+#if defined(__SAML21E17B__)
+	if (sercom_index >= 0 && sercom_index < 5) {
 		MCLK->APBCMASK.reg |= 1 << (sercom_index + MCLK_APBCMASK_SERCOM0_Pos);
-	} else {
+	} else if (sercom_index == 5) {
 		MCLK->APBDMASK.reg |= MCLK_APBDMASK_SERCOM5;
 	}
+#elif defined(__SAMD51N20A__)
+	if (sercom_index >= 0 && sercom_index < 2) {
+		MCLK->APBAMASK.reg |= 1 << (sercom_index + MCLK_APBAMASK_SERCOM0_Pos);
+	} else if (sercom_index >= 2 && sercom_index < 4) {
+		MCLK->APBBMASK.reg |= 1 << (sercom_index + MCLK_APBBMASK_SERCOM2_Pos);
+	} else if (sercom_index >= 4 && sercom_index <= 7) {
+		MCLK->APBDMASK.reg |= 1 << (sercom_index + MCLK_APBDMASK_SERCOM4_Pos);
+	}
+#else
+ "Part not supported!"
+#endif
 }
 
 void sercom_set_gclk_core(int sercom_index, int gclk_gen) {
@@ -77,21 +91,31 @@ void sercom_set_gclk_core(int sercom_index, int gclk_gen) {
 }
 
 void sercom_set_gclk_slow(int sercom_index, int gclk_gen) {
-	static bool sercom5_gclk_slow_initialized;
 	static bool sercom_gclk_slow_initialized;
 
-	bool *initialized;
+	bool *initialized = NULL;
 	int gclk_id;
 
-	if (sercom_index != 5) {
+
+#if defined(__SAML21E17B__)
+	static bool sercom5_gclk_slow_initialized;
+	if (sercom_index >= 0 && sercom_index < 5) {
 		gclk_id = SERCOM0_GCLK_ID_SLOW;
 		initialized = &sercom_gclk_slow_initialized;
-	} else {
+	} else if (sercom_index == 5) {
 		gclk_id = SERCOM5_GCLK_ID_SLOW;
 		initialized = &sercom5_gclk_slow_initialized;
 	}
+#elif defined(__SAMD51N20A__)
+	if (sercom_index >= 0 && sercom_index < SERCOM_INST_NUM) {
+		gclk_id = SERCOM0_GCLK_ID_SLOW;
+		initialized = &sercom_gclk_slow_initialized;
+	}
+#else
+#error "Part not supported!"
+#endif
 
-	if (!(*initialized)) {
+	if (initialized && !(*initialized)) {
 		// disable peripheral clock channel
 		GCLK->PCHCTRL[gclk_id].reg &= ~GCLK_PCHCTRL_CHEN;
 		while (GCLK->PCHCTRL[gclk_id].reg & GCLK_PCHCTRL_CHEN);
