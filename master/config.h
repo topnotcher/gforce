6880f5472e191800f3972704960b4146b8dedb08
Greg Bowser <topnotcher@gmail.com>
2020-03-09 20:47:25 -0400

    config: add GPIO pin assignments.
    I'll probably use these later.

--- master/config.h
+++ master/config.h
@@ -7,6 +7,47 @@
 void configure_pinmux(void);
 
 /**
+ * GPIO assignments
+ */
+typedef enum {
+	PIN_SD_POWER_OFF = PIN_PA07,
+
+	// Xbee pins
+	PIN_XBEE_SLEEP_REQUEST = PIN_PA18,
+	PIN_XBEE_AWAKE = PIN_PA19,
+	PIN_XBEE_RST = PIN_PC21,
+	PIN_XBEE_TCP_CONN = PIN_PC20,
+	PIN_XBEE_TX = PIN_PC17,
+
+	// Charger GPIO
+	PIN_CHG_GP0 = PIN_PC15,
+	PIN_CHG_GP1 = PIN_PC14,
+	PIN_CHG_GP2 = PIN_PC13,
+
+	PIN_VIBRATOR = PIN_PA27,
+
+	// Pack power off
+	PIN_PACK_POWER_OFF = PIN_PB04,
+
+	// Power switch state
+	PIN_PACK_ON = PIN_PB05,
+
+	// External power connected
+	PIN_EXTPWR = PIN_PB06,
+
+	// LED controller power switch
+	PIN_LED_CONTROLLER_OFF = PIN_PB14,
+
+	// audio mute
+	PIN_MUTE = PIN_PB20,
+
+	// Phaser receiver enable
+	PIN_PH_RX_DISABLE = PIN_PB17,
+
+} GpioPin;
+
+
+/**
  * MPC TWI Configuration
  */
 #define MPC_TWI TWIC
