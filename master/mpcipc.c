c6f2a4817eee19c2704619899191c1db58529e35
Greg Bowser <topnotcher@gmail.com>
2020-03-25 10:18:55 -0400

    mpcipc: Copy transmitted packets to avoid losing them.
    We lose custody of the buffer when the send function returns, thus a
    copy must be made.

--- master/mpcipc.c
+++ master/mpcipc.c
@@ -1,5 +1,7 @@
 #include <stdint.h>
 #include <stdlib.h>
+#include <string.h>
+
 #include <malloc.h>
 
 #include "mpcipc.h"
@@ -19,9 +21,16 @@ mpc_driver_t *mpc_loopback_init(uint8_t addr, uint8_t addrmask) {
 }
 
 void mpc_loopback_send(mpc_driver_t *driver, const uint8_t addr, const uint8_t size, uint8_t *const buf) {
+
+	// We need to copy this to an RX buffer because tx_complete() will free buf.[
+	uint8_t buf_size = 0;
+	uint8_t *tx_buf = driver->alloc_buf(&buf_size);
+	uint8_t tx_size = (size > buf_size) ? buf_size : size;
+	memcpy(tx_buf, buf, tx_size);
+
 	struct mpc_rx_data rx_data = {
-		.size = size,
-		.buf = buf,
+		.size = tx_size,
+		.buf = tx_buf,
 	};
 
 	xQueueSend(driver->rx_queue, &rx_data, 0);
